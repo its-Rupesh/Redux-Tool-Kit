3fb25c6ab37609f3be034daec1947214ff7429b8
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-11-27 22:35:39 +0530

    Reducer is Register in Store

--- Rtk/src/redux/store.js
+++ Rtk/src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterReducer from "../features/counter/counterSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer, // Counter Reducer is created ,store can have many reducer of Different name of which they are created
+  },
 });
