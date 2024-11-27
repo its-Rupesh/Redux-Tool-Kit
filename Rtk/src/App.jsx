5add311a7a3fd3b97c87c9d4ca87ea83af256438
its-Rupesh <rupeshchimankare2004@gmail.com>
2024-11-27 22:43:54 +0530

    Buttons and Useselector is created for fetching state stored in store

--- Rtk/src/App.jsx
+++ Rtk/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  return <div>Hello </div>;
+  const count = useSelector((state) => state.counter.value);
+  function handleIncrement() {}
+  function handlerDecrement() {}
+  return (
+    <div className="container">
+      <button onClick={handleIncrement}>+</button>
+      <p>Count:</p>
+      <button onClick={handlerDecrement}>-</button>
+    </div>
+  );
 };
 
 export default App;
